f21bf8a7677b23cbf7c70709fbfca64de7782b83
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-07 16:51:58 -0700

    stuck in Car.py, shit

--- OOP/Ferd.py
+++ OOP/Ferd.py
@@ -4,11 +4,20 @@ class Car(object):
 		self.speed = speed
 		self.fuel = fuel
 		self.mileage = str(mileage)+"mpg"
-		self.tax =  if(self.price>10000):
-						print self.price * (1.15)
-					else:
-						print self.price * (1.12)
+		if self.price>10000:
+			self.tax = (self.price * (1.15))
+		else:
+			self.tax = (self.price * (1.12))
+		self.display_all = display_all()
+			return self.price,self.speed,self.fuel,self.mileage,self.tax
+			return self	
 
-Ferd = Car(15000, 500, "full", 1)
 
-print Ferd 
+megaFerd = Car(15000, 500, "full", 1)
+halfFerd = Car(9000, 120, "almost full", 22)
+doubleFerdly = Car(100000, 320, "fullish", 86)
+fakeCar = Car(8500, 160, "half full", 30)
+littleBigBear = Car(10, 25, "on fumes", 10)
+realCar = Car(9500, 120, "overfilled", 15)
+
+display.all
