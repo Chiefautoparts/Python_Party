2760119dbd87a4291f42c5ed88eeda3a9de694c1
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-07 22:12:39 -0700

    started products.py

---
+++ OOP/product.py
@@ -0,0 +1,17 @@
+class products(object):
+	def __init__(self, price, item_name, weight, brand, cost):
+		self.price = price
+		self.item_name = item_name
+		self.weight = weight
+		self.brand = brand
+		self.cost = cost
+		self.status = "for sale"
+	def sell():
+		self.status = "sold"
+		print self.status
+		return self
+	def add_tax():
+
+	def Return():
+
+	def display():
