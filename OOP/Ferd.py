bf06149c7d4529cc7ab6d66f2884f332ca3d86d7
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-07 17:22:56 -0700

    Ferd, classified as 8 tanks by the government

--- OOP/Ferd.py
+++ OOP/Ferd.py
@@ -1,16 +1,24 @@
 class Car(object):
 	def __init__(self, price, speed, fuel, mileage):
+		self.name = self
 		self.price = price
 		self.speed = speed
 		self.fuel = fuel
 		self.mileage = str(mileage)+"mpg"
 		if self.price>10000:
-			self.tax = (self.price * (1.15))
+			self.tax = "0.15"
 		else:
-			self.tax = (self.price * (1.12))
-		self.display_all = display_all()
-			return self.price,self.speed,self.fuel,self.mileage,self.tax
-			return self	
+			self.tax = "0.12"
+ 
+	def display_all(self):
+		print " "	
+		print "Price: " + str(self.price)
+		print "Speed: " + str(self.speed)
+		print "Fuel: " + str(self.fuel)
+		print "Mileage: " + str(self.mileage)
+		print "Tax: " + str(self.tax)
+		
+
 
 
 megaFerd = Car(15000, 500, "full", 1)
@@ -20,4 +28,9 @@ fakeCar = Car(8500, 160, "half full", 30)
 littleBigBear = Car(10, 25, "on fumes", 10)
 realCar = Car(9500, 120, "overfilled", 15)
 
-display.all
+megaFerd.display_all()
+halfFerd.display_all()
+doubleFerdly.display_all()
+fakeCar.display_all()
+littleBigBear.display_all()
+realCar.display_all()
