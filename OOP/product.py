411fb2933cb594e754788f00c1408df38290fae4
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-08 10:28:43 -0700

    errors for daysgit add .git add .

--- OOP/product.py
+++ OOP/product.py
@@ -1,17 +1,22 @@
 class products(object):
-	def __init__(self, price, item_name, weight, brand, cost):
+	def __init__(self, price, item_name, weight, brand):
 		self.price = price
 		self.item_name = item_name
 		self.weight = weight
 		self.brand = brand
-		self.cost = cost
+		self.cost = self.add_tax
 		self.status = "for sale"
-	def sell():
+	def sell(self):
 		self.status = "sold"
 		print self.status
 		return self
-	def add_tax():
+	def add_tax(self):
+		# self.cost * (1.12)
+		print "Cost after tax is " + self.cost*(1.12)
+		return self
+	'''def returned():
 
-	def Return():
+	def display():'''
+bike = products(50, "party bike", 3, "awesome")
 
-	def display():
+bike.add_tax()
