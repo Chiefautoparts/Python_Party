cbde02663c09d1043a3efa31520087ad3eb601a4
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-07 19:08:38 -0700

    worked on some functions

--- fundamentals/FunFunctions.py
+++ fundamentals/FunFunctions.py
@@ -1,8 +1,14 @@
-a =
-def odd_even(a):
-	for i in range(a):
-		if(i%2 == 0):
-			print "Number is" i + "." + " This is an even number."
+
+'''def odd_even(a,b,c):
+	for c in range(a,b):
+		if(c%2 == 0):
+			print "Number is " + str(c) + "." + " This is an even number."
 		else:
-			print "Number is" i + "." + " This is an odd number."
-print odd_even(1,2001)
+			print "Number is " + str(c) + "." + " This is an odd number."
+odd_even(1,2000,1)'''
+
+def multipy(x, len(x):
+	math = []
+	for x in range(1,(len(x)-1)):
+		math.push(x*5)
+	print math
