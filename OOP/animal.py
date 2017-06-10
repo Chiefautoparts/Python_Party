ba0d7b5d3950db027f803be49b6a19d286635d9d
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-10 15:22:44 -0700

    done with animals assignment

--- OOP/animal.py
+++ OOP/animal.py
@@ -17,23 +17,35 @@ class animal(object):
 		print "Current health is : " + str(self.health)
 		return self
 class dog(animal):
-	def __init__(self):
-		super(animal,self).__init__()
-		self.health = 150
+	def __init__(self, name,health):
+		super(animal,self,'name',health).__init__(name='name',health=150)
+		# self.health = 150
 	def pet(self):
 		self.health += 5
 		print self.health
 		return self
 
+class dragon(animal):
+	def __init__(self,name,health):
+		super(animal,self,'name',health).__init__(name='name',health=170)
+		self.health = 170
+	def fly(self):
+		self.health -= 10
+		print self.health
+		return self
+	def display_health(self):
+		print str(self.health) + " I AM A DRAGON!!!!"
 
+manBearPig = animal('superCereal', 50000) 
 
-boneHead = dog()
+manBearPig.display_health()
+
+boneHead = dog('Loggies')
 
 boneHead.walk().walk().walk().run().run().pet().display_health()
 
-# Squeeagle = animal()
 
-# Squeeagle = animal('Squeeagle', 100);
+Squeeagle = animal('Squeeagle', 100);
 
-# Squeeagle.walk().walk().walk().run().run().display_health()
+Squeeagle.walk().walk().walk().run().run().display_health()
 
