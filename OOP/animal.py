76c8f23c9b898a76548e40b9cff62a82d8fb1f3d
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-10 14:40:12 -0700

    added class DOG

--- OOP/animal.py
+++ OOP/animal.py
@@ -16,10 +16,24 @@ class animal(object):
 	def display_health(self):
 		print "Current health is : " + str(self.health)
 		return self
+class dog(animal):
+	def __init__(self):
+		super(animal,self).__init__()
+		self.health = 150
+	def pet(self):
+		self.health += 5
+		print self.health
+		return self
+
+
+
+boneHead = dog()
+
+boneHead.walk().walk().walk().run().run().pet().display_health()
 
 # Squeeagle = animal()
 
-Squeeagle = animal('Squeeagle', 100);
+# Squeeagle = animal('Squeeagle', 100);
 
-Squeeagle.walk().walk().walk().run().run().display_health()
+# Squeeagle.walk().walk().walk().run().run().display_health()
 
