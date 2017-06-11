97bab77fff363244e10fdb32108eb26d8f6018bf
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-10 22:48:56 -0700

    added some shit, doesjt work

---
+++ OOP/callCenter.py
@@ -0,0 +1,11 @@
+class call(object):
+	def __init__(self, Idnum, name, phoneNum, time, reason):
+		self.Idnum = Idnum
+		self.name = 'name'
+		self.phoneNum = phoneNum
+		self.time = time
+		self.reason = 'reason'
+
+	def display(self):
+		print self.Idnum,self.name,self.phoneNum,self.time,self.reason
+		return self
