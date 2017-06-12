c26086166ebb1d52d608e336e4b2bcce1689857c
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-12 08:44:39 -0700

    forgot to add and commit last night

--- flask_fundamentals/whatsName/Name.py
+++ flask_fundamentals/whatsName/Name.py
@@ -5,14 +5,5 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route('/index', methods=['POST'])
-def process(name):
-	name=request.form['name']
-	return redirect('/')
-
-@app.route('/process/<name>')
-def show_name(name):
-	print name
-	return render_template('process.html')
 
 app.run(debug=True)
