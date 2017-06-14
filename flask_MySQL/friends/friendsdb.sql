0b3a15b1be2662d872424ee9b17e52f03b646210
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-14 04:41:18 -0700

    its a SQL to the SQLs SQL

--- flask_MySQL/friends/friendsdb.sql
+++ flask_MySQL/friends/friendsdb.sql
@@ -2,5 +2,3 @@ INSERT INTO friends (first_name, last_name, age, created_at, updated_at)
 VALUES ("Max", "Patel", "5", NOW(), NOW());
 INSERT INTO friends (first_name, last_name, age, created_at, updated_at)
 VALUES ("Chris", "Jun", "10", NOW(), NOW());
-
-SELECT * FROM friends;
--- flask_MySQL/friends/server.py
+++ flask_MySQL/friends/server.py
@@ -1,28 +1,34 @@
-from flask import Flask, render_template, redirect, request, session, flash
+from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 
 app = Flask(__name__)
 app.secret_key = 'rudy was offsides'
 mysql = MySQLConnector(app, 'friendsdb')
+
 @app.route('/')
 def index():
-	friends = mysql.query_db('SELECT * FROM friends')
-	print friends
-	return render_template('index.html')
+	query = 'SELECT * FROM friends'
+	friends = mysql.query_db(query)
+	return render_template('index.html', all_friends=friends)
+
 @app.route('/friends', methods=['POST'])
 def create():
-	print request.form['first_name']
-	print request.form['last_name']
-	print request.form['age']
-	# return redirect('/')
+# print request.form['first_name']
+# print request.form['last_name']
+# print request.form['age']
+# return redirect('/')
 # @app.route('/friends', methods=['POST'])
-# def create():
-	query = 'INSERT INTO friends (first_name, last_name, age, created_at, updated_at) VALUES (:first_name, :last_name, :age, NOW(), NOW())'
-	data = {
-			'first_name': request.form['first_name'],
-			'last_name': request.form['last_name'],
-			'age': request.form['age']
-	}
-	mysql.query_db(query, data)
-	return redirect('/')
+
+		query = 'INSERT INTO friends (first_name, last_name, age, created_at, updated_at) VALUES (:first_name, :last_name, :age, NOW(), NOW())'
+		data = {
+ 			'first_name': request.form['first_name'],
+ 			'last_name': request.form['last_name'],
+ 			'age': request.form['age']
+ 			}
+		mysql.query_db(query, data)
+		return redirect('/')
+
 app.run(debug=True)
+
+
+
