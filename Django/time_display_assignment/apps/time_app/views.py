a5952a4076cd0c0eedcc1aeb6f4761776e89c619
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-20 01:57:47 -0700

    stuck on date

--- Django/time_display_assignment/apps/time_app/views.py
+++ Django/time_display_assignment/apps/time_app/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import datetime
 
 # Create your views here.
+def index(request):
+	time = {
+	'datetime':'datetime'
+	}
+	return render(request, 'time_app/index.html', time)
