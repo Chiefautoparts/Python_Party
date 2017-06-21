1b532f36d2ebf9a67d5b33da5eaf4988ad4427a5
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-20 21:28:36 -0700

    now ninjagold dont work'

--- Django/NinjaGold/apps/gold_app/views.py
+++ Django/NinjaGold/apps/gold_app/views.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 def index(request):
+	try:
+		request.session['Moneys']
+	except:
+		request.session['Moneys'] = 0
+
+	try:
+		request.session['Messages']
+	except:
+		request.session['Messages'] = []
+
+	if 	
 	return render(request, 'gold_app/index.html')
-
-def dolladollabill(request):
-	
-
-# Create your views here.
