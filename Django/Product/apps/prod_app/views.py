add940e3352a6d1bde37ce903fd2e9925fc3b6a4
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-22 16:50:29 -0700

    stuff n things done

--- Django/Product/apps/prod_app/views.py
+++ Django/Product/apps/prod_app/views.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Product
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-        return render(request, 'prod_app/index.html')
+	context = {
+		'products': Product.objects.all()
+	}
+        return render(request, 'prod_app/index.html', context)
+
+def create_product(request):
+	return request.method
+	return render(request, 'prod_app/index.html')
