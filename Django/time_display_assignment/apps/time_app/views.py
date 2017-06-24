3b0930031c7b8eee90bf29fb43c4d4d3f786701f
Chiefautoparts <chiefautoparts@outlook.com>
2017-06-23 21:38:19 -0700

    time display

--- Django/time_display_assignment/apps/time_app/views.py
+++ Django/time_display_assignment/apps/time_app/views.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponse
-import datetime
+# from django.shortcuts import render, HttpResponse
+# import datetime
 
-# Create your views here.
+# # Create your views here.
+# def index(request):
+# 	return render(request, 'time_app/index.html')
+	
+
+
+from django.shortcuts import render, HttpResponse
 def index(request):
-	time = {
-	'datetime':'datetime'
-	}
-	return render(request, 'time_app/index.html', time)
+  context = {
+  "time":"H:i"
+  }
+  return render(request,'time_app/index.html', context)
+
+
+	# time = {
+	# 'datetime':'datetime'
+	# }
+	# return render(request, 'time_app/index.html', time)
